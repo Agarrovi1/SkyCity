5bd6c4e1aaf9f2ae2cd8cec8c79c6714e8ca3e42
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2020-01-28 13:49:47 -0500

    adds LoginVC

---
+++ SkyCity2/LoginVC.swift
@@ -0,0 +1,18 @@
+//
+//  LoginVC.swift
+//  SkyCity2
+//
+//  Created by Angela Garrovillas on 1/28/20.
+//  Copyright © 2020 Angela Garrovillas. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
